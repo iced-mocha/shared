5df8ba0ebfdadf00b0e0bb112943ad5e437e7ca8
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-10-23 22:20:02 -0700

    Add name to user model

--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 type User struct {
 	ID         string `json:"id"`
+	Name       string `json:"name"`
 	Username   string `json:"username"`
 	RedditUser string `json:"reddit-user"`
 }
