4dfd9764f98d35dac5fb816f196b2288f0648c2a
Jonathon Gore <jackgore@hotmail.ca>
2017-10-29 16:10:14 -0700

    Updated user model to match database schema

--- models/user.go
+++ models/user.go
@@ -6,7 +6,9 @@ type User struct {
 	// TODO: Break into First and Last name?
 	Name string `json:"name"`
 
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	RedditUser string `json:"reddit-user"`
+	Username          string `json:"username"`
+	Password          string `json:"password"`
+	RedditUsername    string `json:"reddit-username"`
+	RedditAuthToken   string `json:"reddit-auth-token"`
+	RedditTokenExpiry int64  `json:"reddit-token-expiry"`
 }
