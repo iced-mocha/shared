bc1bad28acda4fd3200bff278e2b70aed92b3d61
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-12-13 08:28:53 -0800

    Add rss settings to user

--- models/weights.go
+++ models/weights.go
@@ -2,9 +2,10 @@ package models
 
 // Used for unmarshaling weightings for different clients
 type Weights struct {
-	Reddit     float64 `json:"reddit"`
-	Facebook   float64 `json:"facebook"`
-	HackerNews float64 `json:"hacker-news"`
-	GoogleNews float64 `json:"google-news"`
-	Twitter    float64 `json:"twitter"`
+	Reddit     float64            `json:"reddit"`
+	Facebook   float64            `json:"facebook"`
+	HackerNews float64            `json:"hacker-news"`
+	GoogleNews float64            `json:"google-news"`
+	Twitter    float64            `json:"twitter"`
+	RSS        map[string]float64 `json:"rss"`
 }
