968b2c611ed02dc41a99c8b02cacb04f9176d271
Jonathon Gore <jackgore@hotmail.ca>
2017-12-16 18:21:00 -0800

    Added reddit refresh token

--- models/user.go
+++ models/user.go
@@ -6,16 +6,17 @@ type User struct {
 	// TODO: Break into First and Last name?
 	Name string `json:"name"`
 
-	Username          string              `json:"username"`
-	Password          string              `json:"password"`
-	TwitterUsername   string              `json:"twitter-username"`
-	TwitterAuthToken  string              `json:"twitter-auth-token"`
-	TwitterSecret     string              `json:"twitter-secret"`
-	RedditUsername    string              `json:"reddit-username"`
-	RedditAuthToken   string              `json:"reddit-auth-token"`
-	RedditTokenExpiry int64               `json:"reddit-token-expiry"`
-	FacebookUsername  string              `json:"facebook-username"`
-	FacebookAuthToken string              `json:"facebook-auth-token"`
-	RssGroups         map[string][]string `json:"rss-groups"`
-	PostWeights       Weights             `json:"post-weights"`
+	Username           string              `json:"username"`
+	Password           string              `json:"password"`
+	TwitterUsername    string              `json:"twitter-username"`
+	TwitterAuthToken   string              `json:"twitter-auth-token"`
+	TwitterSecret      string              `json:"twitter-secret"`
+	RedditUsername     string              `json:"reddit-username"`
+	RedditAuthToken    string              `json:"reddit-auth-token"`
+	RedditRefreshToken string              `json:"reddit-refresh-token"`
+	RedditTokenExpiry  int64               `json:"reddit-token-expiry"`
+	FacebookUsername   string              `json:"facebook-username"`
+	FacebookAuthToken  string              `json:"facebook-auth-token"`
+	RssGroups          map[string][]string `json:"rss-groups"`
+	PostWeights        Weights             `json:"post-weights"`
 }
